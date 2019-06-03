6e412edf177405b0ff2ff4f898bbc6a44b9a0046
Johannes PHASHA <jphasha@c5r2s1.wethinkcode.co.za>
2019-06-03 12:45:26 +0200

    ft_strncpy.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -17,7 +17,7 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 	size_t i;
 
 	i = 0;
-	if (src[i] != '\0' && (i < len))
+	while (src[i] != '\0' && i < len)
 	{
 		dst[i] = src[i];
 		i++;
