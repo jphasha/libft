c740140ea777c857a18ca714c7dff89ccd8a5321
Johannes PHASHA <jphasha@c5r1s2.wethinkcode.co.za>
2019-06-08 15:44:44 +0200

    ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,30 @@
+#include "libft.h"
+
+void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
+{
+	unsigned char *pdst;
+	unsigned char *psrc;
+	unsigned char oc;
+	size_t dstit;
+	size_t srcit;
+
+	pdst = (unsigned char *)dst;
+	psrc = (unsigned char *)src;
+	oc = c;
+	dstit = 0;
+	while (pdst[dstit])
+	{
+		if (psrc[srcit] && srcit < n && dstit < n)
+		{
+			if (pdst[dstit] == oc)
+			{
+				return (dst);
+			}
+			pdst[dstit] = psrc[srcit];
+		}
+		dstit++;
+		srcit++;
+	}
+	pdst[dstit] = '\0';
+	return (dst);
+}
