17ae8c39febac45af4c66b286fe21e9960613618
Johannes PHASHA <jphasha@c5r1s2.wethinkcode.co.za>
2019-06-08 14:20:17 +0200

    ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	unsigned char *pdst;
+	unsigned char *psrc;
+	size_t dstit;
+	size_t srcit;
+
+	pdst = (unsigned char *)dst;
+	psrc = (unsigned char *)src;
+	dstit = 0;
+	srcit = 0;
+	while (pdst[dstit] && psrc[srcit] && dstit < n)
+	{
+		pdst[dstit] = psrc[srcit];
+		dstit++;
+		srcit++;
+	}
+	pdst[dstit] = '\0';
+	return (&dst[dstit]);
+}
