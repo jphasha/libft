67d82441771c1ba41418a5cdac69c7ad7493e15c
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-06-14 15:42:54 +0200

    ft_memccpy.c alignment fix

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,38 +6,35 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 09:21:17 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/10 09:21:21 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/14 15:41:58 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	unsigned char *pdst;
-	unsigned char *psrc;
-	unsigned char oc;
-	size_t dstit;
-	size_t srcit;
+	unsigned char	*pdst;
+	unsigned char	*psrc;
+	unsigned char	oc;
+	size_t			i;
 
 	pdst = (unsigned char *)dst;
 	psrc = (unsigned char *)src;
 	oc = c;
-	dstit = 0;
-	srcit = 0;
-	while (pdst[dstit])
+	i = 0;
+	while (pdst[i])
 	{
-		if (psrc[srcit] && srcit < n && dstit < n)
+		if (psrc[i] && i < n)
 		{
-			if (pdst[dstit] == oc)
+			if (pdst[i] == oc)
 			{
 				return (dst);
 			}
-			pdst[dstit] = psrc[srcit];
+			pdst[i] = psrc[i];
 		}
-		dstit++;
-		srcit++;
+		i;
 	}
-	pdst[dstit] = '\0';
+	pdst[i] = '\0';
 	return (dst);
 }
