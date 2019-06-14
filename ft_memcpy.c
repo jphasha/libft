662f3a681db05bbdc7dac0ec85ea0ede988fa90e
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-06-14 15:36:51 +0200

    ft_memcpy.c alignment fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 09:20:55 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/10 10:29:57 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/14 15:35:27 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	unsigned char *pdst;
-	unsigned char *psrc;
-	size_t dstit;
-	size_t srcit;
+	unsigned char	*pdst;
+	unsigned char	*psrc;
+	size_t			dstit;
+	size_t			srcit;
 
 	pdst = (unsigned char *)dst;
 	psrc = (unsigned char *)src;
