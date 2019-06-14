12e00fb4e372c1467b60b229fd03e01da3a8d1f3
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-06-14 16:09:52 +0200

    ft_memalloc.c alignment fix

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 14:12:28 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/13 16:22:11 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/14 16:08:34 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*ft_memalloc(size_t size)
 {
-	size_t i;
-	char *mem;
+	size_t	i;
+	char	*mem;
 
 	i = 0;
 	mem = (char *)malloc(size + 1);
