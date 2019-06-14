ac6fed192242d9655773e5b4b42e2a596d44414c
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-06-14 15:52:17 +0200

    ft_memcmp.c alignment fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 09:19:38 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/10 09:42:21 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/14 15:49:39 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *ps1;
-	unsigned char *ps2;
-	size_t s1it;
-	size_t s2it;
+	unsigned char	*ps1;
+	unsigned char	*ps2;
+	size_t			s1it;
+	size_t			s2it;
 
 	ps1 = (unsigned char *)s1;
 	ps2 = (unsigned char *)s2;
