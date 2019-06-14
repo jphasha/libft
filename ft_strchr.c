93fbb003bd533d5c313834366f45ceae605a6ec3
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-06-14 16:02:06 +0200

    ft_strchr.c alignment fix

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 16:34:52 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/07 09:48:04 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/14 16:00:53 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	char *caster;
-	char oc;
-	size_t oppos;
-	size_t firstocc;
+	char	*caster;
+	char	oc;
+	size_t	oppos;
+	size_t	firstocc;
 
 	caster = (char *)s;
 	oc = (char)c;
