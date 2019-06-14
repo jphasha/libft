6158a4f2b6ce2d621100f81996aeb3683bdb968d
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-06-14 14:07:16 +0200

    ft_strdel.c first take

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 23:24:42 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/14 10:59:03 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/14 14:04:48 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr(int n);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_strclr(char *s);
+void	ft_strdel(char **as);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_bzero(void *s, size_t n);
