ec5cd4e706f65009e69b02a5d80c25839a660b87
Johannes PHASHA <jphasha@c5r1s5.wethinkcode.co.za>
2019-06-17 08:54:16 +0200

    ft_itoa.c len + atoi mod

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 08:10:48 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/17 08:26:10 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/17 08:53:42 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 char	*ft_itoa(int n)
 {
 	char	*str;
+	size_t		i;
+	size_t		numlen;
 
+	numlen = ft_strlen(ft_atoi(n));
 	str = (char *)malloc(sizeof(char) * (numlen + 1));
 	if (str == NULL)
 	{
 		return (NULL);
+	}
+	if (n < 0)
+	{
+		n = n * -1;
+		str[i] = '-';
 	}
 
