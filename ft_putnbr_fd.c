41df1f50d082ac5b6eaf34f8533989c6b042fcd4
Johannes PHASHA <jphasha@c5r1s5.wethinkcode.co.za>
2019-06-18 10:39:28 +0200

    ft_putnbr_fd.c update

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 14:19:45 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/13 14:46:53 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/18 10:36:11 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n >= 0 && n <= 9)
+	if (n == -2147483648)
 	{
-		ft_putchar_fd(n + '0', fd);
-	}
-	else if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(n * -1, fd);
+		ft_putstr_fd("-2147483648", fd);
 	}
 	else
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n = n * -1;
+		}
+		else if (n >= 10)
+		{
+			ft_putnbr_fd(n / 10, fd);
+		}
+		ft_putchar_fd((n % 10) + 48, fd);
 	}
 }
