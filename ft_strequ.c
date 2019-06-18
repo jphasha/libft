c995674e2bbd26e9d19a43970450cb94a6f54394
Johannes PHASHA <jphasha@c5r1s5.wethinkcode.co.za>
2019-06-18 11:02:45 +0200

    ft_strequ.c p

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 08:41:59 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/17 09:49:29 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/18 11:00:07 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,21 @@ int	ft_strequ(char const *s1, char const *s2)
 	int i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[j] != '\0')
+	if (!s1 || !s2)
 	{
-		while (s1[i] == s2[i])
+		return (0);
+	}
+	else if (ft_strlen(s1) != ft_strlen(s2))
+	{
+		return (0);
+	}
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
 		{
-			if (s1[i + 1] == '\0' && s2[i + 1] == '\0')
-			{
-				return (1);
-			}
+			return (0);
 		}
 		i++;
 	}
-	return (0);
+	return (1);
 }
