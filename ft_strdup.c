0afdccb1cecabfea6f1af1f0e60048636a711408
Johannes PHASHA <jphasha@c5r1s5.wethinkcode.co.za>
2019-06-18 16:26:54 +0200

    ft_strdup.c f

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 10:17:24 by event             #+#    #+#             */
-/*   Updated: 2019/06/14 15:53:25 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/18 16:25:31 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,12 @@ char	*ft_strdup(const char *src)
 	int		i;
 	char	*srccpy;
 
-	srccpy = (char *)malloc(sizeof(char *) * (ft_strlen(src) + 1));
 	i = 0;
-	if (src[i] == '\0')
+	srccpy = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (srccpy == NULL)
 	{
 		return (NULL);
 	}
-	while (src[i] != '\0')
-	{
-		ft_strcpy(srccpy, src);
-	}
+	ft_strcpy(srccpy, src);
 	return (srccpy);
 }
