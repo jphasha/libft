99602a3540fa1a8248a1b3f72857cb9799c0c8cd
Johannes PHASHA <jphasha@c5r1s5.wethinkcode.co.za>
2019-06-18 09:25:05 +0200

    ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 15:10:38 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/14 16:25:04 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/18 09:23:20 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	newstr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (newstr == NULL)
+	if (s1 && s2)
 	{
-		return (NULL);
+		newstr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2));
+		if (newstr == NULL)
+		{
+			return (NULL);
+		}
+		ft_strcpy(newstr, s1);
+		ft_strcat(newstr, s2);
+		return (newstr);
 	}
-	while (s1[i] != '\0')
-	{
-		newstr[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		newstr[i] = s2[j];
-		i++;
-		j++;
-	}
-	newstr[i] = '\0';
-	return (newstr);
+	return (NULL);
 }
