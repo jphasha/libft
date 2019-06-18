9d430a2e235ec3074d63651facf983ee6547ca18
Johannes PHASHA <jphasha@c5r1s5.wethinkcode.co.za>
2019-06-18 11:15:48 +0200

    ft_strnequ.c p

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 09:24:38 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/17 10:04:19 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/18 11:09:51 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,20 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 	i = 0;
 	if (s1 == NULL || s2 == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
-	while (n > i && (s1[i] != '\0' || s2[i] != '\0'))
+	else if (ft_strlen(s1) != ft_strlen(s2))
 	{
-		while (s1[i] == s2[i])
-		{
-			if (s1[i + 1] == '\0' && s2[i + 1] == '\0')
-			{
-				return (1);
-			}
-			i++;
-		}
+		return (0);
 	}
-	return (0);
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (0);
+		}
+		i++;
+		n--;
+	}
+	return (1);
 }
