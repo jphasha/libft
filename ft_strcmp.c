678df1164f1b233e6fa4255326a41bf7471d1278
Johannes PHASHA <jphasha@c5r1s5.wethinkcode.co.za>
2019-06-19 16:55:39 +0200

    ft_strcmp.c & ft_strncmp.c casting update

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,9 +6,11 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 15:17:27 by jphasha           #+#    #+#             */
-/*   Updated: 2019/05/24 15:25:13 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/19 16:52:38 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
@@ -19,7 +21,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 		i++;
 	}
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 11:45:54 by jphasha           #+#    #+#             */
-/*   Updated: 2019/05/24 14:27:40 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/19 16:54:14 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 		i++;
 	}
