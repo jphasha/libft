38e78714c16b52e50ddbdecc147a60fb51d18660
Johannes PHASHA <jphasha@c5r1s5.wethinkcode.co.za>
2019-06-24 17:09:34 +0200

    ft_itoa.c update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 08:10:48 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/22 11:38:15 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/24 17:08:34 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@ static size_t	ft_num_len(int n)
 {
 	size_t		i;
 
-	i = 1;
-	while (n /= 10)
+	i = 0;
+	if (n < 0)
 	{
+		n = n * -1
+		i++;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
 		i++;
 	}
 	return (i);
@@ -32,23 +38,23 @@ char			*ft_itoa(int n)
 
 	numlen = ft_num_len(n);
 	nbr = n;
+
 	if (n < 0)
 	{
 		nbr = -n;
 		numlen++;
 	}
-	if (!(str = ft_strnew(numlen)))
+	if (!(str = (char *)malloc(sizeof(char) * (numlen + 1))))
 	{
 		return (NULL);
 	}
-	str[numlen--] = nbr % 10 + '0';
-	while (nbr /= 10)
+	i = numlen + 1;
+	str[i] = '\0';
+	while (nbr > 0 && i >= 0)
 	{
-		str[numlen--] = nbr % 10 + '0';
-	}
-	if (n < 0)
-	{
-		str[0] = '-';
+		i--;
+		str[i] = (nbr % 10) + '0';
+		nbr = nbr / 10;
 	}
 	return (str);
 }
