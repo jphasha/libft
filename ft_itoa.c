f81d1cd7540feec7283125de6eb1b106a998a471
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-06-25 12:38:29 +0200

    ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,58 +6,47 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 08:10:48 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/24 17:51:12 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/25 11:56:18 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_num_len(int n)
+static size_t		ft_num_len(int n)
 {
-	size_t		i;
+	size_t	i;
 
-	i = 0;
-	if (n < 0)
-	{
-		n = n * -1;
+	i = 1;
+	while (n /= 10)
 		i++;
-	}
-	while (n > 0)
-	{
-		n = n / 10;
-		i++;
-	}
 	return (i);
 }
 
-char			*ft_itoa(int n)
+char				*ft_itoa(int n)
 {
-	char		*str;
-	size_t		numlen;
-	size_t		nbr;
+	char				*str;
+	size_t				len;
+	unsigned int		nbr;
 
-	numlen = ft_num_len(n);
+	len = ft_num_len(n);
 	nbr = n;
-	if (!(str = (char *)malloc(sizeof(char) * (numlen + 1))))
+	if (n < 0)
+	{
+		nbr = -n;
+		len++;
+	}
+	if (!(str = ft_strnew(len)))
 	{
 		return (NULL);
 	}
-	str[numlen--] = '\0';
-	if (n == 0)
+	str[--len] = nbr % 10 + '0';
+	while (nbr /= 10)
 	{
-		str[0] = '0';
-		return (str);
+		str[--len] = nbr % 10 + '0';
 	}
-	if (nbr < 0)
+	if (n < 0)
 	{
-		str[0] = '-';
-		nbr = nbr * -1;
-	}
-	while (nbr > 0)
-	{
-		str[numlen] = (n % 10) + '0';
-		n = n / 10;
-		numlen--;
+		*(str + 0) = '-';
 	}
 	return (str);
 }
