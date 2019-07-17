6c605de038f3a0e7dabce83e4065c3d37a7f8035
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-07-17 08:19:40 +0200

    ft_lstnew.c complete

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/13 00:41:06 by jphasha           #+#    #+#             */
-/*   Updated: 2019/07/13 02:08:48 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/07/17 08:14:32 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*new_link;
+
 	if ((new_link = (t_list *)malloc(sizeof(*new_link) * content_size)) == NULL)
 	{
 		return (NULL);
@@ -24,6 +25,16 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 		new_link->content = NULL;
 		new_link->content_size = 0;
 	}
-	new_link->new = NULL;
+	else
+	{
+		if ((new_link->content = malloc(content_size)) == NULL)
+		{
+			free(new_link);
+			return (NULL);
+		}
+		ft_memcpy(new_link->content, content, content_size);
+		new_link->content_size = content_size;
+	}
+	new_link->next = NULL;
 	return (new_link);
 }
