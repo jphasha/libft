f6132045a8baf29d517d7fe6dc1c52ce68f8b3b8
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-07-22 09:21:00 +0200

    push

--- ft_merge_sort.c
+++ ft_merge_sort.c
@@ -1,36 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bubble_sort.c                                      :+:      :+:    :+:   */
+/*   ft_merge_sort.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/12 11:30:51 by jphasha           #+#    #+#             */
-/*   Updated: 2019/07/22 09:18:43 by jphasha          ###   ########.fr       */
+/*   Created: 2019/07/22 09:03:59 by jphasha           #+#    #+#             */
+/*   Updated: 2019/07/22 09:05:46 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bubble_sort(int num_arr[], int num_elm)
-{
-	int i;
-	int swpr;
-	int swapped;
 
-	i = 0;
-	while ((i < num_elm - 1))
-	{
-		swapped = 0;
-		if (num_arr[i] > num_arr[i + 1])
-		{
-			ft_swap(&num_arr[i], &num_arr[i + 1]);
-			swapped = 1;
-		}
-		i++;
-		if (swapped == 1)
-		{
-			i = 0;
-		}
-	}
-}
