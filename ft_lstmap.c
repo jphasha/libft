3888e8fdd60d03bfd02603c6d9067b53a3cf21cf
Johannes PHASHA <jphasha@c4r4s2.wethinkcode.co.za>
2019-08-24 17:10:45 +0200

    very incomplete

---
+++ ft_lstmap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/24 16:25:56 by jphasha           #+#    #+#             */
+/*   Updated: 2019/08/24 17:10:03 by jphasha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*map_lnk;
+	t_list	*map_lnk_i;
+
+	if (!lst || !f)
+		return (NULL);
+	map_lnk_i = lst
+	while (map_lnk)
+	{
+		map_lnk = f(map_lnk_i);
+		map_lnk_i = map_lnk_i->next;
+	}
+	free(map_lnk_i);
+	return (map_lnk);
+}
