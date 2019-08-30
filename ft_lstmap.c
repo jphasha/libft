a2f803476cdaa309c9ca6d1673c3ff7bc5933582
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-08-30 14:10:22 +0200

    ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/24 16:25:56 by jphasha           #+#    #+#             */
-/*   Updated: 2019/08/30 13:52:28 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/08/30 14:09:52 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,17 @@ t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 
 	if (!lst || !f)
 		return (NULL);
-	map_lnk_i = lst;
-	while (map_lnk)
+	map_lnk_i = f(lst);
+	map_lnk = map_lnk_i;
+	while (lst->next)
 	{
-		map_lnk = f(map_lnk_i);
+		lst = lst->next;
+		if (!(map_link_i->next = f(lst)))
+		{
+			free(map_link_i->next);
+			return (NULL);
+		}
 		map_lnk_i = map_lnk_i->next;
 	}
-	free(map_lnk_i);
 	return (map_lnk);
 }
